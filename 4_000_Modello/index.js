8f2d1d4e8c0bf0834f9c6cd3d67bc24aec9d3593
raccaPaolo <p.racca.1368@vallauri.edu>
2020-11-21 11:44:50 +0100

    Updated default index.js

--- 4_000_Modello/index.js
+++ 4_000_Modello/index.js
@@ -1,13 +1,7 @@
 "use strict"
 
-window.onload = function(){
+$(document).ready(function(){
+})
 
-}
 
-function generaNumero(min,max){
-    // formula per generare numeri tra min e max, estremi inclusi
-    // math.floor tronca il numero con la virgola all'intero più basso (approssimo per difetto)
-    let n = Math.floor((max-min+1)*Math.random())+min  
-    return n;
-}
 
